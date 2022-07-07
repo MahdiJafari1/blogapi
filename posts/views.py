a640216b5a416ac6084b8e452a5f2acdd2dfbf67
Mahdi Jafari <mahdi100th@gmail.com>
2022-07-07 15:22:25 +0430

    User Authentication

---
+++ posts/views.py
@@ -0,0 +1,16 @@
+from rest_framework import generics
+from.permissions import IsAuthorOrReadOnly
+
+from .models import Post
+from .serializers import PostSerializer
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
